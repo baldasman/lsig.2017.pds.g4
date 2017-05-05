b1423dd02ef7e8d7e31d92f4546d574f88ce5ff7
emanuelgarrido <ema.afife@gmail.com>
2017-05-05 19:14:57 +0100

    [PG4-35]

--- JoiaDaEuropa/app/controllers/teste_controller.rb
+++ JoiaDaEuropa/app/controllers/teste_controller.rb
@@ -1,3 +1,44 @@
 class TesteController < ApplicationController
 
+  before_action :check_account
+
+  def index
+
+    @client = current_user.client
+
+  end
+
+  def edit_client
+
+    @client = current_user.client
+
+  end
+
+  def save_client
+
+    @client = current_user.client
+    @client.name = params[:client][:name]
+
+    if @client.save
+      redirect_to client_area_index_path
+    else
+      flash[:error] = @client.errors.messages
+
+      redirect_to client_area_edit_account_path
+    end
+
+  end
+
+  private
+
+  def check_client
+
+    @client = Client.find_by(user_id: current_user.id)
+
+    if @client.nil?
+      current_user.client = Client.new name: current_user.email
+      current_user.save
+    end
+
+  end
 end
