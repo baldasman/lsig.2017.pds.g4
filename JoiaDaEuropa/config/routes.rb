ac0625bde658f3fe32e00de133a706ea7f296f28
emanuelgarrido <ema.afife@gmail.com>
2017-05-09 12:12:24 +0100

    [PG4-36] - S

--- JoiaDaEuropa/config/routes.rb
+++ JoiaDaEuropa/config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/profile/index'
   get '/client_area/index'
+  get '/new_order/index'
 
   get '/client_area/edit_client'
   patch '/client_area/save_client'
+
+  get '/new_order/edit_order'
+  patch '/new_order/save_order'
 
   root to: 'home#index'
   
