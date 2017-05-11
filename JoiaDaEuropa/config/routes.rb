aa9af8ae38a84b7eedfa3e2a897bf3a4e8592165
emanuelgarrido <ema.afife@gmail.com>
2017-05-11 13:57:29 +0100

    [PG4-38] - S [PG4-39]

--- JoiaDaEuropa/config/routes.rb
+++ JoiaDaEuropa/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/profile/index'
+  get '/home/index'
   get '/client_area/index'
   get '/new_order/index'
   get '/pending_order/index'
