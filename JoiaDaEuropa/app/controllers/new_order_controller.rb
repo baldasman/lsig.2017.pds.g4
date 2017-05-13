d3113184ad48c69cff4ce650fa0458ecc8a716f8
emanuelgarrido <ema.afife@gmail.com>
2017-05-13 15:29:49 +0100

    [PG4-39]

--- JoiaDaEuropa/app/controllers/new_order_controller.rb
+++ JoiaDaEuropa/app/controllers/new_order_controller.rb
@@ -11,36 +11,26 @@ class NewOrderController < ApplicationController
 
 end
 
-def edit_order
-
-  @order = Order.new
-
-end
-
 def save_order
-
-  @order = Order.new
-
+@order = Order.new
     #Descrição
-    @order.description = params[:order][:description]
+@order.description = params[:order][:description]
 
     #Data_Entrega
-    @order.delivery_date = params[:order][:delivery_date]
+@order.delivery_date = params[:order][:delivery_date]
 
     #Preço_Unitario
-    @order.unit_price = params[:order][:unit_price]
+@order.unit_price = params[:order][:unit_price]
 
     #Quantidade
-    @order.quantity = params[:order][:quantity]
+@order.quantity = params[:order][:quantity]
 
     #Estado
-    @order.state_id = params[:order][:state_id]
-
-
-
-  if @order.save
-    redirect_to new_order_index_path
-  end
+@order.state_id = params[:order][:state_id]
 
   end
+
+  #if @order.save     redirect_to new_order_index_path  end
 end
+
+
