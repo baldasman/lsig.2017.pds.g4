e503c9f6dd89aa2572904d06da4d930f2d3ff199
adrianapereira <adrianapsalonso@hotmail.com>
2017-05-15 10:57:59 +0100

    [PG4-18]

---
+++ JoiaDaEuropa/app/controllers/end_order_controller.rb
@@ -0,0 +1,10 @@
+class EndOrderController < ApplicationController
+  def index
+
+    @order = Order.find_by(params[:id])
+
+    #where @order.state_id("Pendente")
+
+    #if description.nil?      show_message "There is no orders"    end
+  end
+end
