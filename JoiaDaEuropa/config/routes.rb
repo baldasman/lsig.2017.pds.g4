61e6bd43cff73dfab9ca0d6bb79446a235ee8131
emanuelgarrido <ema.afife@gmail.com>
2017-05-17 10:52:29 +0100

    [PG4-41] S

--- JoiaDaEuropa/config/routes.rb
+++ JoiaDaEuropa/config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   get '/client_area/edit_client'
   patch '/client_area/save_client'
 
-  get '/order/index'
+  get '/order/update'
+  get '/order/destroy'
   get '/order/new'
   get '/order/show'
   post '/order/create'
